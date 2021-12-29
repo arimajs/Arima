9d548070e0450b14f6e4e26c23c533c7f792205a
Lioness100 <jchickmm2@gmail.com>
2021-12-28 19:58:37 -0500

    refactor(constants): :zap: refactor rootURL to use /pieces `getRootData`

--- src/lib/utils/constants.ts
+++ src/lib/utils/constants.ts
@@ -1,7 +1,8 @@
+import { URL, pathToFileURL } from 'node:url';
+import { getRootData } from '@sapphire/pieces';
 import { Constants } from 'discord.js';
-import { URL } from 'node:url';
 
-export const rootURL = new URL('../../../', import.meta.url);
+export const rootURL = new URL('./', pathToFileURL(getRootData().root));
 
 export enum BrandingColors {
 	Primary = Constants.Colors.AQUA,
