906b4ed091a5d33863f4716557f7e441bee9dfee
Lioness100 <jchickmm2@gmail.com>
2022-03-05 22:39:25 -0500

    feat(entities): :sparkles: create User entity

--- src/lib/database/DatabaseManager.ts
+++ src/lib/database/DatabaseManager.ts
@@ -2,14 +2,17 @@ import type { EntityManager, EntityRepository, MongoDriver } from '@mikro-orm/mo
 import { dbOptions } from '#root/config';
 import { MikroORM } from '@mikro-orm/core';
 import { Member } from '#entities/Member';
+import { User } from '#entities/User';
 
 export class DatabaseManager {
 	public readonly em: EntityManager;
 	public readonly members: EntityRepository<Member>;
+	public readonly users: EntityRepository<User>;
 
 	private constructor(public readonly orm: MikroORM<MongoDriver>) {
 		this.em = orm.em;
 		this.members = orm.em.getRepository(Member);
+		this.users = orm.em.getRepository(User);
 	}
 
 	public static async connect() {
---
+++ src/lib/database/entities/User.ts
@@ -0,0 +1,40 @@
+import type { HexColorString, Snowflake } from 'discord.js';
+import { SerializedPrimaryKey, PrimaryKey, Entity, Property, OptionalProps, Embedded, Embeddable } from '@mikro-orm/core';
+
+@Embeddable()
+export class Track {
+	@Property()
+	public track!: string;
+
+	@Property()
+	public thumbnail?: string;
+
+	@Property()
+	public color?: HexColorString;
+}
+
+@Embeddable()
+export class Playlist {
+	@Property()
+	public name!: string;
+
+	@Embedded()
+	public tracks: Track[] = [];
+
+	@Property()
+	public createdAt: Date = new Date();
+}
+
+@Entity()
+export class User {
+	public [OptionalProps]?: 'playlists';
+
+	@PrimaryKey({ type: 'string' })
+	public _id!: Snowflake;
+
+	@SerializedPrimaryKey({ type: 'string' })
+	public id!: Snowflake;
+
+	@Embedded(() => Playlist, { array: true })
+	public playlists: Playlist[] = [];
+}
