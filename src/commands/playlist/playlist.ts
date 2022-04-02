19a92cf1cf0ab00b7e25fb06411c89bc3e72df3f
Lioness100 <jchickmm2@gmail.com>
2022-04-02 19:18:59 -0400

    feat(playlist): :sparkles: implement /playlist delete
    #untested

--- src/commands/playlist/playlist.ts
+++ src/commands/playlist/playlist.ts
@@ -4,6 +4,9 @@ import { UseRequestContext } from '#utils/decorators';
 import { ArimaCommand } from '#structures/ArimaCommand';
 import { ApplyOptions } from '@sapphire/decorators';
 import { pluralize } from '#utils/common';
+import { ObjectId } from '@mikro-orm/mongodb';
+import { italic } from '@discordjs/builders';
+import { from } from '@sapphire/framework';
 
 @ApplyOptions<ArimaCommand.Options>({
 	description: 'Execute functions related to cutsom playlists!'
@@ -41,6 +44,25 @@ export class UserCommand extends ArimaCommand {
 		await interaction.reply({ embeds: [embed] });
 	}
 
+	public async delete(interaction: ArimaCommand.Interaction) {
+		const playlist = interaction.options.getString('playlist', true);
+
+		// The id is validated here and the "creator" property is added to the query because in theory the use can
+		// ignore the autocomplete options and either input something that's not an ObjectId (24 character hex string),
+		// or an ObjectId that corresponds to a playlist that's not theirs.
+		const _id = from(() => new ObjectId(playlist));
+
+		// If `new ObjectId(...)` threw an error, exit early. Otherwise, send a delete query, which will return
+		// the number of "affected rows" (documents deleted). If the number is 0, the playlist doesn't exist.
+		const deleted = _id.success && (await this.container.db.playlists.nativeDelete({ _id: _id.value, creator: interaction.user.id }));
+		if (!deleted) {
+			return sendError(interaction, `That playlist doesn't exist!\n${italic("💡Make sure you're using the autocomplete menu")}`);
+		}
+
+		const embed = createEmbed('✅ Successfully deleted playlist!');
+		await interaction.reply({ embeds: [embed] });
+	}
+
 	public override registerApplicationCommands(registry: ArimaCommand.Registry) {
 		registry.registerChatInputCommand((builder) =>
 			builder
@@ -55,6 +77,18 @@ export class UserCommand extends ArimaCommand {
 								.setName('name')
 								.setDescription("The name of the playlist! (You won't be able to edit this later)")
 								.setRequired(true)
+						)
+				)
+				.addSubcommand((subcommand) =>
+					subcommand
+						.setName('delete')
+						.setDescription('Delete a custom playlist!')
+						.addStringOption((option) =>
+							option
+								.setName('playlist')
+								.setDescription('The name of the playlist you want to delete!')
+								.setRequired(true)
+								.setAutocomplete(true)
 						)
 				)
 		);
